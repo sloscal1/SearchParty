93d5b2031957a6a36dc0714b6d215c51893e2854
sloscal1 <sloscalzo85@gmail.com>
2015-11-08 20:37:18 -0500

    Working through the Dispatcher process.
    Added some more comments on the Dispatcher line, and also to the
    setup.proto file. I believe the message system in that file is
    sufficient for a first crack to get the work moving. Actually, I think I
    need to specify the DB path!

--- HorizonExperimenter/src/core/party/Dispatcher.java
+++ HorizonExperimenter/src/core/party/Dispatcher.java
@@ -1,8 +1,16 @@
 package core.party;
 
 public class Dispatcher {
-	//Take in the setup message
+	//Takes the setup message and results message.
+	//Check to see if this a continuation
+	
 	//Figure out all the experiments
+	
 	//Set up the data base
+	//.Check to see if it already exists.
+	//.Check to see if there are runs that match this codebase and parameter settings
+	//TODO Check in this code and see if there are any changes between it other experiments.
+	//TODO Maybe it would be easier to simply request the user to specify an experiment to continue (from DB)
+	
 	//Set up the Searchers
 }
